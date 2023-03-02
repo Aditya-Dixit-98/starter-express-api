e7656c20d6b63b696577ec4fa236eb9da1bc9ad6
Aditya-Dixit-98 <43183429+Aditya-Dixit-98@users.noreply.github.com>
2023-03-02 14:02:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require('express')
+const { Configuration, OpenAIApi } = require("openai");
+const cors = require('cors')
+require('dotenv').config();
+app.use(cors())
+app.use(express.json());
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
 const app = express()
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+    openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: req.body.prompt,
+      }).then(data => {
+        res.send(data.data.choices[0])
+      }).catch(error => {
+        console.error(error)
+      })
+  })
 })
 app.listen(process.env.PORT || 3000)
